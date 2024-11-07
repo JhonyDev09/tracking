a0e8b5a8c5cad35eb7ac80c369b5b4911465232e
JhonyDev009 <jhonatanolivar09@gmail.com>
2024-11-07 14:33:30 -0600

    Resuelto conflicto de fusión en datos.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,25 +8,25 @@ import { DispositivosModule } from './dispositivos/dispositivos.module';
 import { DatosModule } from './datos/datos.module';
 import { Dispositivo } from './dispositivos/entities/dispositivo.entity';
 import { Dato } from './datos/entities/dato.entity';
-//import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'database-1.ctwks8qa0png.us-east-2.rds.amazonaws.com',
       port: 3306,
-      username: 'root',
-      password: '',
+      username: 'admin',
+      password: 'olivarsoto09',
       database: 'tracking',
       entities: [Dispositivo, Dato],
       synchronize: true,
     }),
-    //ConfigModule.forRoot({
-      //isGlobal: true, // Hace que ConfigModule esté disponible en toda la aplicación sin necesidad de importarlo en cada módulo
-      //envFilePath: '.env', // Ruta al archivo .env (por defecto ya busca .env en el root)
-    //}),
+    ConfigModule.forRoot({
+      isGlobal: true, // Hace que ConfigModule esté disponible en toda la aplicación sin necesidad de importarlo en cada módulo
+      envFilePath: '.env', // Ruta al archivo .env (por defecto ya busca .env en el root)
+    }),
     UsuariosModule,
     UnidadesModule,
     DispositivosModule,
