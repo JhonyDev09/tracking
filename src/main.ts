fbb18c17499c259a6dbb91d1ebe968146612cfab
JhonyDev009 <jhonatanolivar09@gmail.com>
2024-11-26 23:26:26 -0600

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000', // Permitir el frontend desde este origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Si necesitas enviar cookies o autenticación
+  });
+
   // Obtener el puerto desde las variables de entorno, con 3000 como valor por defecto
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
