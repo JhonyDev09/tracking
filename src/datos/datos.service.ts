4890a9e30a31328add276077f6b822b29e22e545
JhonyDev009 <jhonatanolivar09@gmail.com>
2024-12-03 01:01:09 -0600

    Correxion envio de datos mapa v1.2

--- src/datos/datos.service.ts
+++ src/datos/datos.service.ts
@@ -17,17 +17,28 @@ export class DatosService {
 
   async obtenerUltimoDato(): Promise<any> {
     const ultimoDato = await this.datoRepository
-    .createQueryBuilder('dato')
-    .innerJoinAndSelect('dato.dispositivo', 'dispositivo')       
-    .innerJoinAndSelect('dispositivo.unidad', 'unidad')       
-    .innerJoinAndSelect('unidad.dispunidad', 'dispunidad')          
-    .innerJoinAndSelect('dispunidad.chofer', 'empleado')            
-    .innerJoinAndSelect('unidad.usrunidad', 'usrunidad')            
-    .innerJoinAndSelect('usrunidad.usuario', 'usuario')
-    .select(['dispositivo.id AS id_dispositivo', 'dispositivo.numSerie AS numSerieDisp', 'dato.imei AS imei','dato.latitud AS latitud', 'dato.longitud AS longitud', 'unidad.placas AS placas', 'unidad.numSerie AS numSerie', 'empleado.nombre AS nombre', 'empleado.apellidos AS apellidos', 'empleado.numTel As numTel', 'dato.fechahra AS fechahra'])           
-    .orderBy('dato.fechahra', 'DESC')                               
-    .limit(1)                                               
-    .getRawOne();
+      .createQueryBuilder('dato')
+      .innerJoinAndSelect('dato.dispositivo', 'dispositivo')       // Relación con Dispositivo
+      .innerJoinAndSelect('dispositivo.dispunidad', 'dispunidad')   // Relación con DispUnidad
+      .innerJoinAndSelect('dispunidad.unidad', 'unidad')           // Relación con Unidad
+      .innerJoinAndSelect('unidad.usrunidad', 'usrunidad')         // Relación con UsrUnidad
+      .innerJoinAndSelect('usrunidad.empleado', 'empleado')        // Relación con Empleado
+      .select([
+        'dispositivo.id AS id_dispositivo', 
+        'dispositivo.numSerie AS numSerieDisp', 
+        'dato.imei AS imei',
+        'dato.latitud AS latitud', 
+        'dato.longitud AS longitud', 
+        'unidad.placas AS placas', 
+        'unidad.numSerie AS numSerie', 
+        'empleado.nombre AS nombre', 
+        'empleado.apellidos AS apellidos', 
+        'empleado.numTel AS numTel', 
+        'dato.fechahra AS fechahra'
+      ])
+      .orderBy('dato.fechahra', 'DESC')                          
+      .limit(1)                                               
+      .getRawOne();              
 
     if (!ultimoDato) {
       console.error('No se encontró el último dato.');
